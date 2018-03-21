9ed77d7c4ca3536502f4313e88745d7ce4105f49
lanstine <laanstine@mavs.coloradomesa.edu>
2018-03-21 10:35:29 -0600

    minor changes to update files

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #ifdef __linux__ 
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL.h>
+	#include <SDL2/SDL_image.h>
+	#include <SDL2/SDL.h>
 #elif _WIN32
-#include <SDL.h>
-#include <SDL_image.h>
+	#include <SDL.h>
+	#include <SDL_image.h>
 #else
 
 #endif
